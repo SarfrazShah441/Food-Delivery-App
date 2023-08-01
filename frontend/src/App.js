547890dde1ef298a82cc1991094f9661fb37beb4
SarfrazShah441 <shahsarfraz441@gmail.com>
2023-08-01 17:06:52 +0500

    finishing header and navbar

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,20 @@
 import React from 'react'
+import { Route, BrowserRouter as Router, Routes } from 'react-router-dom'
+import './styles/app.scss'
+import './styles/header.scss'
+import Header from './components/Layout/Header'
+import Home from './components/Home/Home'
+
 
 const App = () => {
   return (
-    <div>App</div>
+    <Router>
+      <Header />
+      {/* routes go here */}
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </Router>
   )
 }
 
