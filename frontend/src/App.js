d16a4430e1513888101f643496ef981297921371
SarfrazShah441 <shahsarfraz441@gmail.com>
2023-08-09 22:45:42 +0500

    confirm order page UI completed

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -9,14 +9,14 @@ import './styles/footer.scss'
 import './styles/contact.scss'
 import './styles/cart.scss'
 import './styles/shipping.scss'
+import './styles/confirmOrder.scss'
 import Header from './components/Layout/Header'
 import Home from './components/Home/Home'
 import Footer from './components/Layout/Footer'
 import Contact from './components/contact/Contact'
 import Cart from './components/cart/Cart'
 import Shipping from './components/cart/Shipping'
-
-
+import ConfirmOrder from './components/cart/ConfirmOrder'
 const App = () => {
   return (
     <Router>
@@ -27,6 +27,7 @@ const App = () => {
         <Route path="/contact" element={<Contact />} />
         <Route path="/cart" element={<Cart />} />
         <Route path="/shipping" element={<Shipping />} />
+        <Route path="/confirmorder" element={<ConfirmOrder />} />
       </Routes>
       <Footer />
     </Router>
