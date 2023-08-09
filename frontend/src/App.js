0c270ad016cebc945ff937f23b94eb2883f68cc0
SarfrazShah441 <shahsarfraz441@gmail.com>
2023-08-09 20:21:03 +0500

    shipping page UI completed

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -8,11 +8,13 @@ import './styles/menu.scss'
 import './styles/footer.scss'
 import './styles/contact.scss'
 import './styles/cart.scss'
+import './styles/shipping.scss'
 import Header from './components/Layout/Header'
 import Home from './components/Home/Home'
 import Footer from './components/Layout/Footer'
 import Contact from './components/contact/Contact'
 import Cart from './components/cart/Cart'
+import Shipping from './components/cart/Shipping'
 
 
 const App = () => {
@@ -24,6 +26,7 @@ const App = () => {
         <Route path="/" element={<Home />} />
         <Route path="/contact" element={<Contact />} />
         <Route path="/cart" element={<Cart />} />
+        <Route path="/shipping" element={<Shipping />} />
       </Routes>
       <Footer />
     </Router>
